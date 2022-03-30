e6938256e46f3a260ae9d4d8102830d72231c2ba
민규 전 <mktsk2@gmail.com>
2022-03-30 15:37:02 +0900

    메세지3추가

--- GitTest/Form1.cs
+++ GitTest/Form1.cs
@@ -26,5 +26,10 @@ namespace GitTest
         {
             MessageBox.Show("메세지2");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("메세지3");
+        }
     }
 }
